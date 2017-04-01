dca0512834c31a2de186925af29e7d1ad020ab30
Bret Ignoto <bret.ignoto@gmail.com>
2017-03-31 18:20:44 -0700

    babel precompiled

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-// var fs = require('fs');
-// var path = require('path');
-// var express = require('express');
-// var app = express();
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
+var app = express();
 
-// app.use('/', express.static(__dirname));
+app.use('/', express.static(__dirname));
 
-// app.listen(8000, function() {
-//   console.log('Server started: http://localhost:8000/');
-// });
+app.listen(8000, function() {
+  console.log('Server started: http://localhost:8000/');
+});
